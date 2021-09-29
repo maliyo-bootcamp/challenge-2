77aa6cf153454dda61d16aabf421d3a2f19b80eb
auleki <kodagiwa@gmail.com>
2021-09-29 20:40:04 +0100

    Challenge 2 added

---
+++ Assets/Challenge 2/Scripts/DetectCollisionsX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisionsX : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Dog"))
+        {
+            Debug.Log("Dog got the ball");
+            Destroy(gameObject);
+        }
+    }
+}
